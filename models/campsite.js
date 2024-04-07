ca72c3b13e99d241bb2b7b7f549eac9c92e0d321
theannagreen <theannalgreen@gmail.com>
2024-04-07 16:11:55 -0400

    google authentication added

---
+++ models/campsite.js
@@ -0,0 +1,23 @@
+const reviewSchema = new Schema({
+    content: {
+      type: String,
+      required: true
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 5
+    },
+    // Don't forget to add the comma above then
+    // add the 3 new properties below
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    userName: String,
+    userAvatar: String
+  }, {
+    timestamps: true
+  });
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+
+// google oAuth login route 
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email']
+  }
+));
+
+//google oAuth callback route 
+router.get('/auth/google/callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/', // redirect to hompage on successful authentication
+    failureRedirect: '/login' //redirect to login page on authentication 
+  }
+));
+
+// logout route 
+router.get('/logout', function(req, res) {
+  req.logout(); //passport.js adds a logout() function to req object 
+  res.redirect('/'); // redirect to homepage after logout 
+});
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
