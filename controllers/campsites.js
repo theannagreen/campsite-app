c1f2e567871c134df9c111899a782a4fe0049f1d
theannagreen <theannalgreen@gmail.com>
2024-04-12 11:37:39 -0400

    clean up notes and commented out code

--- controllers/campsites.js
+++ controllers/campsites.js
@@ -13,12 +13,9 @@ module.exports = {
 
 async function index(req, res) {
     try {
-        // grab campsites from the database
         const campsites = await Campsite.find();
-        // render index page and pass the campsites data to the template
         res.render('campsites/index', { title: 'All Campsites', campsites: campsites });
     } catch (error) {
-        // error if fetching campsites fails
         res.status(500).send('Error fetching campsites');
     }
 }
@@ -42,7 +39,6 @@ async function create(req, res) {
     console.log('create function called');
     try {
         req.body.campsite
-        //pull campsite data from the request body
         const { campsite, location, longitude, latitude, season, description, mpaaRating } = req.body;
         const newCampsite = new Campsite({
             campsite,
@@ -54,12 +50,9 @@ async function create(req, res) {
             mpaaRating,
             reviews: []
         });
-        // Save the new campsite to the database
         await newCampsite.save();
-        // Redirect to the index page or any other appropriate page
         res.redirect('/campsites');
     } catch (err) {
-        // Handle error if creation fails
         console.error('Error creating new campsite:', error);
         res.status(500).send('Error creating campsite');
     }
@@ -80,7 +73,6 @@ async function update(req, res) {
         campsite.season = req.body.season;
         campsite.description = req.body.description;
 
-        // Save the updated campsite
         await campsite.save();
 
         // Redirect to the campsite show page *all campsites page*
@@ -93,9 +85,7 @@ async function update(req, res) {
 
 async function deleteCampsite(req, res) {
     try {
-        // Delete the campsite from the database
         const campsiteId = req.params.id;
-        // delete the campsite from the database 
         await Campsite.findByIdAndDelete(campsiteId);
         res.redirect('/campsites');
     } catch (error) {
@@ -106,20 +96,13 @@ async function deleteCampsite(req, res) {
 
 async function addReview(req, res) {
     try {
-        // Find the campsite by ID
         const campsite = await Campsite.findById(req.params.id);
-
-        // Add the review to the campsite's reviews array
         campsite.reviews.push({
             userName: req.body.userName,
             rating: req.body.rating,
             content: req.body.content
         });
-
-        // Save the updated campsite
         await campsite.save();
-
-        // Redirect back to the campsite page
         res.redirect(`/campsites/${campsite._id}`);
     } catch (error) {
         console.error('Error adding new review:', error);
